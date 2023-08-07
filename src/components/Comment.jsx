a96e5a114fb6bf26cb79b1609984fec5b759dc16
Yan Valadares <yancardoso@outlook.com.br>
2023-08-07 17:24:12 -0300

    Criação e estilização de novos componentes

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/yan-valadares.png"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Yan Valadares</strong>
+                            <time title='07 de agosto às 14:25' dateTime='2023-08-07 14:25:32'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -10,10 +11,8 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img 
-                className={styles.avatar}
-                src="https://github.com/yan-valadares.png"
-                />
+                <Avatar src="https://github.com/yan-valadares.png"/>
+                
 
                 <strong>Yan Valadares</strong>
                 <span>AS Student</span>
